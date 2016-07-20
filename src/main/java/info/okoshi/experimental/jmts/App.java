f16b04f32078478ef4d1afe889497613f284dba9
okosheep <hiroomi+github@okoshi.net>
2016-07-21 00:04:13 +0900

    fix #1 Create Java project.

---
+++ src/main/java/info/okoshi/experimental/jmts/App.java
@@ -0,0 +1,13 @@
+package info.okoshi.experimental.jmts;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
